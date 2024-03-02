7afe4be140f1c6ce03097b327afb41817a349975
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-03-03 00:02:55 +0530

    Working on Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        {/* <Button className="mt-1">Something</Button> */}
+      </div>
       <ButtonGradient />
     </>
   );
